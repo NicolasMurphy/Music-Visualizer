7c541e77290e0299505f935b11a8cda6238e17ea
Nicolas Murphy <murphynick10@gmail.com>
2024-01-30 12:00:09 -0600

    simplified audio test

--- src/Play.tsx
+++ src/Play.tsx
@@ -1,20 +1,20 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { startAudioDirectly } from './audioSetup';
-import Visualizer from './Visualizer';
+// import Visualizer from './Visualizer';
 
 const Play = () => {
-  const [isAudioReady, setIsAudioReady] = useState(false);
+  // const [isAudioReady, setIsAudioReady] = useState(false);
 
   const handlePlayClick = () => {
     startAudioDirectly();
-    setIsAudioReady(true);
+    // setIsAudioReady(true);
   };
 
   return (
     <div>
       <h1>Music Visualizer</h1>
       <button onClick={handlePlayClick}>Play</button>
-      {isAudioReady && <Visualizer />}
+      {/* {isAudioReady && <Visualizer />} */}
     </div>
   );
 };
--- src/audioSetup.ts
+++ src/audioSetup.ts
@@ -1,31 +1,59 @@
-import { camera } from './sceneSetup.js';
-import { AudioListener, Audio, AudioLoader, AudioAnalyser} from 'three';
+// import { camera } from './sceneSetup.js';
+// import { AudioListener, Audio, AudioLoader, AudioAnalyser} from 'three';
 
+// let started = false;
+// let sound: Audio , analyser: AudioAnalyser;
+
+let audioContext: AudioContext;
 let started = false;
-let sound: Audio , analyser: AudioAnalyser;
 
 const startAudioDirectly = () => {
     if (!started) {
-        if (!sound) {
-            const listener = new AudioListener();
-            camera.add(listener);
-            sound = new Audio(listener);
-            const audioLoader = new AudioLoader();
-
-            audioLoader.load('/cadet(pinballMix).mp3', function(buffer) {
-                sound.setBuffer(buffer);
-                sound.setLoop(true);
-                sound.setVolume(0.5);
-                analyser = new AudioAnalyser(sound, 4096);
-
-                sound.play();
-                started = true;
-            });
+        if (!audioContext) {
+            audioContext = new (window.AudioContext);
+            fetch('/cadet(pinballMix).mp3')
+                .then(response => response.arrayBuffer())
+                .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+                .then(audioBuffer => {
+                    const source = audioContext.createBufferSource();
+                    source.buffer = audioBuffer;
+                    source.connect(audioContext.destination);
+                    source.start();
+                    started = true;
+                })
+                .catch(error => console.error('Error with fetching the audio file:', error));
         } else {
-            sound.play();
+            audioContext.resume();
             started = true;
         }
     }
 };
 
-export { startAudioDirectly, analyser, started };
+
+// const startAudioDirectly = () => {
+//     if (!started) {
+//         if (!sound) {
+//             const listener = new AudioListener();
+//             camera.add(listener);
+//             sound = new Audio(listener);
+//             const audioLoader = new AudioLoader();
+
+//             audioLoader.load('/cadet(pinballMix).mp3', function(buffer) {
+//                 sound.setBuffer(buffer);
+//                 sound.setLoop(true);
+//                 sound.setVolume(0.5);
+//                 analyser = new AudioAnalyser(sound, 4096);
+
+//                 sound.play();
+//                 started = true;
+//             });
+//         } else {
+//             sound.play();
+//             started = true;
+//         }
+//     }
+// };
+
+// export { startAudioDirectly, analyser, started };
+
+export { startAudioDirectly, started };
