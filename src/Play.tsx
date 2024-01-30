0d1f8652267f042463464d9de666eeeedc89a250
Nicolas Murphy <murphynick10@gmail.com>
2024-01-30 10:44:24 -0600

    removed unused import

--- src/Play.tsx
+++ src/Play.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { initializeAudio, startAudio } from './audioSetup';
 import Visualizer from './Visualizer';
 
--- src/Visualizer.tsx
+++ src/Visualizer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { scene, camera, renderer, appendRendererToDOM } from './sceneSetup';
 import { analyser, started } from "./audioSetup";
