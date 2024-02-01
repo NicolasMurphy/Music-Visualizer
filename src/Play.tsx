bb1f35b715d889f6416d9f2c8f1f34b48cc77bcb
Nicolas Murphy <murphynick10@gmail.com>
2024-01-31 21:02:14 -0600

    changed back

--- src/Play.tsx
+++ src/Play.tsx
@@ -1,20 +1,28 @@
-// import { useState } from 'react';
-import { startAudioDirectly } from './audioSetup';
-// import Visualizer from './Visualizer';
+import { useState } from 'react';
+import { initializeAudio, startAudio } from './audioSetup';
+import Visualizer from './Visualizer';
 
 const Play = () => {
-  // const [isAudioReady, setIsAudioReady] = useState(false);
+  const [isSongLoaded, setIsSongLoaded] = useState(false);
+  const [isAudioReady, setIsAudioReady] = useState(false);
 
-  const handlePlayClick = () => {
-    startAudioDirectly();
-    // setIsAudioReady(true);
+  const loadSong = () => {
+    initializeAudio(() => {
+      setIsSongLoaded(true);
+      setIsAudioReady(true);
+    });
   };
 
   return (
     <div>
       <h1>Music Visualizer</h1>
-      <button onClick={handlePlayClick}>Play</button>
-      {/* {isAudioReady && <Visualizer />} */}
+      {!isSongLoaded && <button onClick={loadSong}>Load Song</button>}
+      {isAudioReady && (
+        <>
+          <button onClick={startAudio}>Play Music</button>
+          <Visualizer />
+        </>
+      )}
     </div>
   );
 };
