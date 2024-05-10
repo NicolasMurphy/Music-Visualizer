2eb43e1c40e2d05a2c77992f914c451b8d0e5e1a
Nicolas Murphy <murphynick10@gmail.com>
2024-05-10 16:57:00 -0500

    Revert "Revert "trying out microphone""
    This reverts commit c3e468068b6fe021c74890a23907174b6b1896a1.

--- src/Play.tsx
+++ src/Play.tsx
@@ -1,28 +1,53 @@
-import { useState } from 'react';
-import { initializeAudio, startAudio } from './audioSetup';
+// import { useState } from 'react';
+// import { initializeAudio, startAudio } from './audioSetup';
+// import Visualizer from './Visualizer';
+
+// const Play = () => {
+//   const [isSongLoaded, setIsSongLoaded] = useState(false);
+//   const [isAudioReady, setIsAudioReady] = useState(false);
+
+//   const loadSong = () => {
+//     initializeAudio(() => {
+//       setIsSongLoaded(true);
+//       setIsAudioReady(true);
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <h1>Music Visualizer</h1>
+//       {!isSongLoaded && <button onClick={loadSong}>Load Song</button>}
+//       {isAudioReady && (
+//         <>
+//           <button onClick={startAudio}>Play Music</button>
+//           <Visualizer />
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Play;
+
+import React from 'react';
 import Visualizer from './Visualizer';
 
-const Play = () => {
-  const [isSongLoaded, setIsSongLoaded] = useState(false);
-  const [isAudioReady, setIsAudioReady] = useState(false);
+const Play: React.FC = () => {
+  const [isMicEnabled, setIsMicEnabled] = React.useState(false);
 
-  const loadSong = () => {
-    initializeAudio(() => {
-      setIsSongLoaded(true);
-      setIsAudioReady(true);
-    });
+  const handleEnableMic = () => {
+    setIsMicEnabled(true);
   };
 
   return (
     <div>
-      <h1>Music Visualizer</h1>
-      {!isSongLoaded && <button onClick={loadSong}>Load Song</button>}
-      {isAudioReady && (
-        <>
-          <button onClick={startAudio}>Play Music</button>
-          <Visualizer />
-        </>
+      {/* Button to enable microphone input */}
+      {!isMicEnabled && (
+        <button onClick={handleEnableMic}>Enable Microphone</button>
       )}
+
+      {/* Render the Visualizer component if the microphone is enabled */}
+      {isMicEnabled && <Visualizer />}
     </div>
   );
 };
